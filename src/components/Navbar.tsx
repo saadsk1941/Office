acb90be86d8e2fd9588c3ced45f8956b25a62702
saadsk1941 <saadsageer.sk@gmail.com>
2026-02-21 11:47:20 +0530

    Changed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ function Navbar() {
 
   return (
     <nav className="navbar">
-      <h2 className="logo"><img src="/LGRJ.png" width="25%" /></h2>
+      <h2 className="logo">Raj Computers</h2>
 
       <button
         className={`nav-toggle ${open ? "open" : ""}`}
